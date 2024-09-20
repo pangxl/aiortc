a69b17358edc567b1e10bf0a459651e073e3b463
pangxl <pangxl@nscc-tj.cn>
2024-09-20 10:12:33 +0800

    修改h264比特率

--- src/aiortc/codecs/h264.py
+++ src/aiortc/codecs/h264.py
@@ -16,9 +16,9 @@ from .base import Decoder, Encoder
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_BITRATE = 1000000  # 1 Mbps
-MIN_BITRATE = 500000  # 500 kbps
-MAX_BITRATE = 3000000  # 3 Mbps
+DEFAULT_BITRATE = 5000000  # 1 Mbps
+MIN_BITRATE = 5000000  # 500 kbps
+MAX_BITRATE = 5000000  # 3 Mbps
 
 MAX_FRAME_RATE = 30
 PACKET_MAX = 1300
